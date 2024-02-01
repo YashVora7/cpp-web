23767840f4a6ae2341fc8ffe183dc553010e914a
aryansabhani <112259316+aryansabhani@users.noreply.github.com>
2024-02-01 10:17:43 +0530

    cpp

---
+++ i.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+   int a,b;
+
+
+   cout << "Enter Value of A:";
+   cin >> a;
+
+   cout << "Enter Value of B:";
+   cin >> b;
+
+   int sum = a+b;
+   cout << "Your Sum is:"<< sum;
+
+   return 0;
+
+}
